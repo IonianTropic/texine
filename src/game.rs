34513d9da3b8aac50fb909408936c4949a1f0bcc
Sam Quatrone <samquatrone@gmail.com>
2023-12-24 15:09:51 -0700

    implemented text demo

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,12 @@
 use std::fs;
 
-use sdl2::{render::Canvas, video::Window, event::Event, keyboard::Keycode, EventPump, ttf::Font, pixels::Color};
+use sdl2::{render::Canvas, video::Window, event::Event, keyboard::Keycode, EventPump, ttf::{Font, Sdl2TtfContext}, pixels::Color, render::TextureQuery, rect::Rect};
 
 
 pub struct Game {
     canvas: Canvas<Window>,
     event_pump: EventPump,
+    ttf_context: Sdl2TtfContext,
     _prompt: String,
     _command: String,
 }
@@ -21,27 +22,22 @@ impl Game {
 
         let mut canvas = window.into_canvas().build().unwrap();
         canvas.clear();
-        canvas.present();
-    
+
         let event_pump = sdl_context.event_pump().unwrap();
+        let ttf_context = sdl2::ttf::init().unwrap();
 
-        let sdl_ttf_context = sdl2::ttf::init().unwrap();
-
-        let font = sdl_ttf_context
-            .load_font("./assets/fonts/OpenSans-Regular.ttf", 12)
-            .unwrap();
-        let partial_rendering = font.render("hello, wrold!");
-        partial_rendering.solid(Color::RGB(255, 255, 255)).unwrap();
         Self {
             canvas,
             event_pump,
+            ttf_context, 
             _prompt: String::new(),
             _command: String::new(),
         }
     }
 
     pub fn run(&mut self) {
-        
+        self.text_demo();
+
         'running: loop {
             // print prompt
 
@@ -58,12 +54,39 @@ impl Game {
             // handle user input (update)
 
             // print response
-            self.canvas.clear();
-            self.canvas.present();
+
         }
     }
 
-    fn _print_prompt(prompt: String) {
+    fn text_demo(&mut self) {        
+        // Create surface with desired text
+        let font = self.ttf_context
+            .load_font("./assets/fonts/OpenSans-Regular.ttf", 12)
+            .unwrap();
 
+        let surface = font
+            .render("hello, wrold!")
+            .blended(Color::RGB(255,255,255))
+            .map_err(|e| e.to_string())
+            .unwrap();
+
+        // Convert surface into texture
+        let texture_creator = self.canvas.texture_creator();
+        let texture = texture_creator
+            .create_texture_from_surface(&surface)
+            .map_err(|e| e.to_string())
+            .unwrap();
+
+        // Display texture
+        let TextureQuery {width, height, ..} = texture.query();
+        let target_rect = Rect::new(
+            0,
+            0,
+            width,
+            height
+        );
+
+        self.canvas.copy(&texture, None, target_rect).unwrap();
+        self.canvas.present();
     }
 }
